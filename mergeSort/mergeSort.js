cb4d64ef9f693f16dd5e6b60fe169f3a30add648
Chase Wood <wood.c.chase@gmail.com>
2019-05-01 10:13:58 -0600

    Fixed bugs for mergeSort

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -97,51 +97,34 @@
 
 
 
-var mergeSort = function(array) {
-var mergeSort = function(array) 
-  
-  let len     = array.length,       // number of items in the arrayay 
-      middle,                     // middle of the arrayay
-      left,                       // left side of the arrayay 
-      right,                      // right side of the arrayay 
-
-  // arrayays with 0 or 1 elements don't need sorting
-  if (len < 2) {
+var mergeSort = function(array) { 
+                     
+  if (array.length < 2) {
     return array
   }
+  let middle = Math.floor(array.length/2)
+  let left = array.slice(0, middle) 
+  let right = array.slice(middle) 
 
-  middle = Math.floor(len/2)
-
-
-  left = array.slice(0, middle)   // left side, from 0 to the middle  
-  right = array.slice(middle)     // right side, from the middle to the end
-
-  return merge(MergeSort(left), MergeSort(right))
-
+  return merge(mergeSort(left), mergeSort(right))
 }
 
-
-// Merges 2 sorted arrays
-function merge(left, right) {
+var merge = function(left, right) {
   let result = [],
       i = 0,
       j = 0
-
-  while(i < left.length && j < right.length) {
-
-    // Elements in both arrays are compared against each other.
-    // Whichever element is smaller it's inserted in the results.
-    
+  while(i < left.length && j < right.length) { 
     if(left[i] < right[j]) {
-      result.push(left[i++])       // Each time a value from one array is added, it's 
-    } else {                       // corresponding index variable is incremented.
+      result.push(left[i++])
+    } else {
       result.push(right[j++])
     }
   }
 
-  // As soon as one of the arrays has been finished, the
-  // remaining values are added to the end of the result array
-
   return result.concat(left.slice(i)).concat(right.slice(j))
 
 }
+
+let arr = [4,7,4,3,9,1,2];
+
+let results = mergeSort(arr);
