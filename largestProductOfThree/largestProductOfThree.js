8ac437d310b2c00e949ebfe71795858a3c8bd4b3
Chase Wood <wood.c.chase@gmail.com>
2019-06-05 11:34:59 -0600

    refactored to one-liner

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,35 +8,37 @@
  */
 
 
-var largestProductOfThree = array => {
-  let one, two, three, none, ntwo;
-  //array.filter((val, i, self) => self.length <= 3 ? val : self.forEach((value, idx, arr, acc = 0) => val <= value))
-  let negatives = array.filter(val => val < 0);
-  if (negatives.length >= 2) {
-    negatives.map(val => {
-      if(!none || val < none) {
-      	ntwo = none;
-      	none = val;
-      }
-      else if(!ntwo || val < ntwo) {
-      	ntwo = val;
-      }
-    })
-  }
-  array.map(val => {
-    if(!one || val > one) {
-    	three = two;
-    	two = one;
-    	one = val;
-    }
-    else if(!two || val > two) {
-    	three = two;
-    	two = val;
-    }
-    else if(!three || val > three) three = val;
-  })
-  if(negatives.length === array.length) return one*two*three
-  else if(Math.abs(none*ntwo) > one*two) return none*ntwo*one;
-  else if(Math.abs(none*ntwo) > two*three) return one*none*ntwo
-  return one*two*three;
-};
+// var largestProductOfThree = array => {
+//   let one, two, three, none, ntwo;
+//   const negatives = array.filter(val => val < 0);
+//   if (negatives.length >= 2) {
+//     negatives.map(val => {
+//       if(!none || val < none) {
+//       	ntwo = none;
+//       	none = val;
+//       }
+//       else if(!ntwo || val < ntwo) {
+//       	ntwo = val;
+//       }
+//     })
+//   }
+//   array.map(val => {
+//     if(!one || val > one) {
+//     	three = two;
+//     	two = one;
+//     	one = val;
+//     }
+//     else if(!two || val > two) {
+//     	three = two;
+//     	two = val;
+//     }
+//     else if(!three || val > three) three = val;
+//   })
+//   if(negatives.length === array.length) return one*two*three
+//   else if(Math.abs(none*ntwo) > one*two) return none*ntwo*one;
+//   else if(Math.abs(none*ntwo) > two*three) return one*none*ntwo
+//   return one*two*three;
+// };
+
+//THE ONE LINER
+var largestProductOfThree = arr =>  arr.every(val => val < 0) ? arr.sort((a,b)=>a-b).slice(0, 3).reduce((acc, cur) => acc*cur, 1) : arr.filter(val => val < 0).length >= 2 ? arr.sort((a,b)=>a-b).slice(0, 2).reduce((acc, cur) => acc*cur, 1) > arr.sort((a,b)=>a-b).slice(arr.length-3, arr.length-1).reduce((acc, cur) => acc*cur, 1) ? arr.sort((a,b)=>a-b).slice(0, 2).push(arr[arr.length-1]).reduce((acc, cur) => acc*cur, 1) : arr.sort((a,b)=>a-b).slice(arr.length-3, arr.length).reduce((acc, cur) => acc*cur, 1) : arr.sort((a,b)=>a-b).slice(arr.length-3, arr.length).reduce((acc, cur) => acc*cur, 1)
